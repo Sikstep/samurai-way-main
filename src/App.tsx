743525c524cebfc844cf3e5b3700a430f8c4208c
Sikstep <Sikstep1988@gmail.com>
2023-02-02 16:39:23 +0300

    Lesson 11 SW Verstka

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,50 @@ import './App.css';
 
 
 function App() {
+
     return (
-        <div className="App">
-            <button>MyYouTubeChannel - 1</button>
-            <button>MyYouTubeChannel - 2</button>
+        <div className={'app-wrapper'}>
+            <header className={'header'}>
+                <img
+                    src="https://cdn.logo.com/hotlink-ok/logo-social.png"/>
+            </header>
+            <nav className={'nav'}>
+                <div>
+                    <a>Profile</a>
+                </div>
+                <div>
+                    <a>Messages</a>
+                </div>
+                <div>
+                    <a>News</a>
+                </div>
+                <div>
+                    <a>Music</a>
+                </div>
+                <div>
+                    <a>Settings</a>
+                </div>
+            </nav>
+            <div className={'content'}>
+                <img
+                    src="https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg"/>
+            </div>
+            <div>
+                ava + discription
+                <img src="https://www.tailorbrands.com/wp-content/uploads/2020/07/mcdonalds-logo.jpg" alt=""/>
+            </div>
+            <div>
+                My posts
+                <div>
+                    New post
+                </div>
+                <div>
+                    Post 1
+                </div>
+                <div>
+                    Post 2
+                </div>
+            </div>
         </div>
     );
 }
