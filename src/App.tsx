a9322c544138cce5c910fdfbd4155f49859de6cc
Sikstep <Sikstep1988@gmail.com>
2023-02-02 23:06:20 +0300

    Lesson 12-13 Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,17 @@
 import React from 'react';
 import './App.css';
+import {Header} from './components/Header';
+import {Navbar} from './components/Navbar';
+import {Profile} from './components/Profile';
 
 
 function App() {
 
     return (
         <div className={'app-wrapper'}>
-            <header className={'header'}>
-                <img
-                    src="https://cdn.logo.com/hotlink-ok/logo-social.png"/>
-            </header>
-            <nav className={'nav'}>
-                <div>
-                    <a>Profile</a>
-                </div>
-                <div>
-                    <a>Messages</a>
-                </div>
-                <div>
-                    <a>News</a>
-                </div>
-                <div>
-                    <a>Music</a>
-                </div>
-                <div>
-                    <a>Settings</a>
-                </div>
-            </nav>
-            <div className={'content'}>
-                <img
-                    src="https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg"/>
-            </div>
-            <div>
-                ava + discription
-                <img src="https://www.tailorbrands.com/wp-content/uploads/2020/07/mcdonalds-logo.jpg" alt=""/>
-            </div>
-            <div>
-                My posts
-                <div>
-                    New post
-                </div>
-                <div>
-                    Post 1
-                </div>
-                <div>
-                    Post 2
-                </div>
-            </div>
+            <Header/>
+            <Navbar/>
+            <Profile/>
         </div>
     );
 }
---
+++ src/components/Profile.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+export const Profile = () => {
+    return (
+        <div className={'content'}>
+            <div>
+                <img
+                    src="https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg"/>
+            </div>
+            <div>
+                ava + discription
+                <img src="https://www.tailorbrands.com/wp-content/uploads/2020/07/mcdonalds-logo.jpg" alt=""/>
+            </div>
+            <div>
+                My posts
+                <div>
+                    New post
+                </div>
+                <div>
+                    Post 1
+                </div>
+                <div>
+                    Post 2
+                </div>
+            </div>
+        </div>
+
+    )
+}
