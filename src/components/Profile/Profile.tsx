37a9326ea78e8cafcf51440e03fcaee9fe6a8d4f
Sikstep <Sikstep1988@gmail.com>
2023-02-10 16:49:33 +0300

    Lesson 19 Routing

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -4,8 +4,8 @@ import {MyPosts} from './MyPosts/MyPosts';
 
 export const Profile = () => {
     return (
-        <div className={`${classes.content} ${classes.img}`}>
-            <div>
+        <div>
+            <div className={classes.preview}>
                 <img
                     src="https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg"/>
             </div>
