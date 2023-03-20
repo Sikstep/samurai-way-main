46d226413b97c0b091224197bf7534009df1308f
Sikstep <Sikstep1988@gmail.com>
2023-03-20 16:37:52 +0300

    Lesson 41 started

--- src/redux/Profile-reducer.ts
+++ src/redux/Profile-reducer.ts
@@ -1,33 +1,21 @@
 import {v1} from 'uuid';
-import {StateType, TsarType} from './Types';
+import {profilePageType, StateType, TsarType} from './Types';
 
-export const ProfileReducer = (state: StateType, action: TsarType): StateType => {
+const ProfileReducer = (state: profilePageType, action: TsarType): profilePageType => {
     switch (action.type) {
         case 'ADD-POST':
             let newPost = {
                 id: v1(),
-                title: state.profilePage.newPostText,
+                title: state.newPostText,
                 likeCount: 0,
             };
-            state.profilePage.posts.unshift(newPost);
-            state.profilePage.newPostText = ''
+            state.posts.unshift(newPost);
+            state.newPostText = ''
 
-          return state
+            return state
 
         case 'CHANGE-NEW-TEXT':
-            state.profilePage.newPostText = action.message;
-
-            return state
-
-        case 'CHANGE-NEW-DIALOG-MESSAGE':
-            state.dialogsPage.newMessage = action.newMessage;
-
-            return state
-
-        case 'ADD-DIALOGMESSAGE':
-            let newDialogMessage = {id: v1(), message: state.dialogsPage.newMessage};
-            state.dialogsPage.messages.push(newDialogMessage);
-            state.dialogsPage.newMessage = '';
+            state.newPostText = action.message;
 
             return state
 
@@ -35,4 +23,6 @@ export const ProfileReducer = (state: StateType, action: TsarType): StateType =>
             return state;
     }
 };
+
+export default ProfileReducer;
 
--- src/redux/state.tsx
+++ src/redux/state.tsx
@@ -1,4 +1,4 @@
-import {DispatchType, StoreType, TsarType} from './Types';
+import {StoreType, TsarType} from './Types';
 import {v1} from 'uuid';
 
 export const store: StoreType = {
@@ -80,51 +80,3 @@ export const store: StoreType = {
         }
     },
 }
-
-
-// export let state: StateType = {
-//     profilePage: {
-//         posts: [
-//             {id: 1, title: 'Hi, how are you?', likeCount: 15},
-//             {id: 2, title: `It's my first post`, likeCount: 10},
-//             {id: 3, title: 'Third post', likeCount: 7},
-//             {id: 4, title: 'Fourth post', likeCount: 19},
-//             {id: 5, title: 'Fifth post', likeCount: 2},
-//         ],
-//         newPostText: ''
-//     },
-//     dialogsPage: {
-//         dialogs: [
-//             {id: 1, name: 'Dimych'},
-//             {id: 2, name: 'Andrey'},
-//             {id: 3, name: 'Sveta'},
-//             {id: 4, name: 'Sasha'},
-//             {id: 5, name: 'Viktor'},
-//             {id: 6, name: 'Valera'},
-//         ],
-//         messages: [
-//             {id: 1, message: 'Hi'},
-//             {id: 2, message: 'How is your it-kamasutra?'},
-//             {id: 3, message: 'Yo'},
-//             {id: 4, message: 'Yo'},
-//             {id: 5, message: 'Yo'},
-//         ],
-//     },
-// };
-//
-// export const subscribe = (callBack: () => void) => {
-//     rerenderEntireTree = callBack
-// }
-// export let addPost = () => {
-//
-//     let newPost = {id: state.profilePage.posts.length + 1, title: state.profilePage.newPostText, likeCount: 0};
-//     state.profilePage.posts.push(newPost);
-//     state.profilePage.newPostText = ''
-//     rerenderEntireTree();
-// }
-//
-// export const writeTextPost = (message: string) => {
-//
-//     state.profilePage.newPostText = message;
-//     rerenderEntireTree();
-// }
