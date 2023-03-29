eea8dcb211bcbf2cac600aa9bfc1eda6ee3f73be
Sikstep <Sikstep1988@gmail.com>
2023-03-29 14:38:45 +0300

    Lesson 46 addDeepcopy for reducers, fix bags + refactoring Dialog-reducer.ts + refactoring Profile-reducer.ts

--- src/redux/Profile-reducer.ts
+++ src/redux/Profile-reducer.ts
@@ -2,17 +2,18 @@ import {v1} from 'uuid';
 import {profilePageType, AllACTypes} from './Types';
 
 const initialPostsPageState: profilePageType = {
-        posts: [
-            {id: v1(), title: 'Hi, how are you?', likeCount: 15},
-            {id: v1(), title: `It's my first post`, likeCount: 10},
-            {id: v1(), title: 'Third post', likeCount: 7},
-            {id: v1(), title: 'Fourth post', likeCount: 19},
-            {id: v1(), title: 'Fifth post', likeCount: 2},
-        ],
-        newPostText: ''
-    };
+    posts: [
+        {id: v1(), title: 'Hi, how are you?', likeCount: 15},
+        {id: v1(), title: `It's my first post`, likeCount: 10},
+        {id: v1(), title: 'Third post', likeCount: 7},
+        {id: v1(), title: 'Fourth post', likeCount: 19},
+        {id: v1(), title: 'Fifth post', likeCount: 2},
+    ],
+    newPostText: ''
+};
 
 const ProfileReducer = (state: profilePageType = initialPostsPageState, action: AllACTypes): profilePageType => {
+    let stateCopy;
     switch (action.type) {
         case 'ADD-POST':
             let newPost = {
@@ -20,17 +21,18 @@ const ProfileReducer = (state: profilePageType = initialPostsPageState, action: 
                 title: state.newPostText,
                 likeCount: 0,
             };
-            let stateCopy = {...state, posts: [newPost, ...state.posts], newPostText: ''}
+            stateCopy = {...state, posts: [newPost, ...state.posts], newPostText: ''}
             // state.posts.unshift(newPost);    как было раньше)))
+            // let stateCopy = {...state, posts: [newPost, ...state.posts], newPostText: ''}
             // state.newPostText = ''
 
             return stateCopy
 
         case 'CHANGE-NEW-TEXT':
-            let newPostTextCopy = {...state, newPostText: action.message}
+            // let newPostTextCopy = {...state, newPostText: action.message}
             // state.newPostText = action.message; как было раньше=)
-
-            return newPostTextCopy
+            stateCopy = {...state, newPostText: action.message};
+            return stateCopy
 
         default:
             return state;
