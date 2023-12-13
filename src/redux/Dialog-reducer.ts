5b5d8e25ca4c57c300d9052630dcba751c115fcb
Sikstep <Sikstep1988@gmail.com>
2023-12-13 15:16:21 +0300

    New message

--- src/redux/Dialog-reducer.ts
+++ src/redux/Dialog-reducer.ts
@@ -16,6 +16,7 @@ const initialMessagesPageState: messagesPageType = {
         {id: v1(), message: 'Yo'},
         {id: v1(), message: 'Yo'},
         {id: v1(), message: 'Yo'},
+        {id: v1(), message: 'Hi man!'},
     ],
     newMessage: '',
 };
